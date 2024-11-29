c8393fdaa605072246cd94d9cf6cdcad9fb55227
Dayu Liu <ldydyx@gmail.com>
2024-11-29 15:46:56 +0800

    hide the extended navigation menu if there is a click outside of the component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
@@ -10,22 +10,40 @@ import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
 const Header = () => {
-  const [dropdownOpenIndex, setDropdownOpenIndex] = useState<number | null>(null)
-  const timeoutRef = useRef<number | null>(null)
+  const [activeIndex, setActiveIndex] = useState(null)
+  const [activeSubIndex, setActiveSubIndex] = useState(null)
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
-  const handleMouseEnter = (index: number) => {
-    // Clear any existing timers and open the dropdown
-    if (timeoutRef.current) clearTimeout(timeoutRef.current)
-    setDropdownOpenIndex(index)
+  const handleMainClick = (index) => {
+    setActiveIndex(activeIndex === index ? null : index) // Toggle main dropdown
+    setActiveSubIndex(null) // Close any open submenus
   }
 
-  const handleMouseLeave = () => {
-    // Set a delay before closing the dropdown
-    timeoutRef.current = window.setTimeout(() => {
-      setDropdownOpenIndex(null)
-    }, 200) // 200 ms delay
+  const handleSubClick = (subIndex) => {
+    // if there is no third menu, jump
+    setActiveSubIndex(activeSubIndex === subIndex ? null : subIndex) // Toggle sub-dropdown
   }
 
+  const resetMenu = () => {
+    setActiveIndex(null);
+    setActiveSubIndex(null);
+  };
+
+  // Hide dropdowns on outside click
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        resetMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  
   const headerClass = `flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10 overflow-visible ${
     siteMetadata.stickyNav ? 'sticky top-0 z-50' : ''
   }`
@@ -45,34 +63,64 @@ const Header = () => {
         </div>
       </Link>
       <div className="flex items-center space-x-6 leading-5 sm:space-x-8">
-        <div className="no-scrollbar hidden items-center space-x-4 overflow-visible sm:flex sm:space-x-6">
+        <div ref={menuRef} className="no-scrollbar hidden items-center space-x-4 overflow-visible sm:flex sm:space-x-6">
           {headerNavLinks.map((link, index) => (
-            <div
-              key={link.title}
-              className="relative"
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <Link
-                href={link.href}
-                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-              >
-                {link.title}
-              </Link>
-              {link.hrefs && dropdownOpenIndex === index && (
-                <div
-                  className="absolute top-full z-50 mt-2 flex w-40 flex-col rounded-md bg-white shadow-lg dark:bg-gray-800"
-                  onMouseEnter={() => timeoutRef.current && clearTimeout(timeoutRef.current)}
-                  onMouseLeave={handleMouseLeave}
+            <div key={link.title} className="relative">
+              {/* First Level Main Menu */}
+              {link.hrefs ? (
+                <button
+                  onClick={() => handleMainClick(index)}
+                  className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
                 >
-                  {link.hrefs.map((sublink) => (
-                    <Link
-                      key={sublink.title}
-                      href={sublink.href}
-                      className="block px-4 py-2 font-medium text-gray-700 hover:bg-primary-500 dark:text-gray-100 dark:hover:bg-primary-400"
-                    >
-                      {sublink.title}
-                    </Link>
+                  {link.title}
+                </button>
+              ) : (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                >
+                  {link.title}
+                </Link>
+              )}
+
+              {/* Second Level Dropdown */}
+              {link.hrefs && activeIndex === index && (
+                <div className="absolute top-full z-50 mt-2 flex w-40 flex-col rounded-md bg-white shadow-lg dark:bg-gray-800">
+                  {link.hrefs.map((secondLink, subIndex) => (
+                    <div key={secondLink.title} className="relative">
+                      {secondLink.hrefs ? (
+                        <button
+                          onClick={() => handleSubClick(subIndex)}
+                          className="block w-full px-4 py-2 text-left font-medium text-gray-700 hover:bg-primary-500 dark:text-gray-100 dark:hover:bg-primary-400"
+                        >
+                          {secondLink.title}
+                        </button>
+                      ) : (
+                        <Link
+                          key={secondLink.title}
+                          href={secondLink.href}
+                          className="block px-4 py-2 font-medium text-gray-700 hover:bg-primary-500 dark:text-gray-100 dark:hover:bg-primary-400"
+                        >
+                          {secondLink.title}
+                        </Link>
+                      )}
+
+                      {/* Third Level Dropdown */}
+                      {secondLink.hrefs && activeSubIndex === subIndex && (
+                        <div className="top-0 z-50 mt-0 flex w-40 flex-col rounded-md bg-white shadow-lg dark:bg-gray-800">
+                          {secondLink.hrefs.map((thirdLink) => (
+                            <Link
+                              key={thirdLink.title}
+                              href={thirdLink.href}
+                              className="block px-4 py-2 font-medium text-gray-700 hover:bg-primary-500 dark:text-gray-100 dark:hover:bg-primary-400"
+                            >
+                              &nbsp;&#10551;&nbsp;{thirdLink.title}
+                            </Link>
+                          ))}
+                        </div>
+                      )}
+                    </div>
                   ))}
                 </div>
               )}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/about', title: 'Home' },
   { href: '/latest', title: 'News' },
-  { href: '/publication',
+  {
+    href: '/publication',
     title: 'Publications',
     // hrefs: [
     //   { title: 'OPEN-SOURCE SOFTWARE TOOLS', href: '/topics/next-js' },
@@ -9,21 +10,64 @@ const headerNavLinks = [
     //   { title: 'PROTOCOLS', href: '/tags/github' },
     // ]
   },
-  { href: '/resource/open-source-software-tools',
+  {
+    href: '/resource/open-source-software-tools',
     title: 'Resources',
     hrefs: [
-      { title: 'OPEN-SOURCE SOFTWARE TOOLS', href: '/resource/open-source-software-tools' },
-      { title: 'OPEN-SOURCE DATABASES', href: '/resource/open-source-databases' },
-      { title: 'PROTOCOLS AND INITIATIVES', href: '/resource/protocols-and-initiatives' },
-    ]
+      {
+        title: 'OPEN-SOURCE SOFTWARE TOOLS',
+        href: '/resource/open-source-software-tools',
+        hrefs: [
+          { title: 'OPTICAL COHERENCE TOMOGRAPHY ANGIOGRAPHY', href: '/resource/open-source-software-tools/optical-coherence-tomography-angiography' },
+          {
+            title: 'ANTERIOR CHAMBER',
+            href: '/resource/open-source-software-tools/anterior-chamber',
+          },
+        ],
+      },
+      {
+        title: 'OPEN-SOURCE DATABASES',
+        href: '/resource/open-source-databases',
+        hrefs: [
+          { title: 'RETINA',
+            href: '/resource/open-source-databases/retina'
+          },
+          {
+            title: 'ANTERIOR CHAMBER',
+            href: '/resource/open-source-databases/anterior-chamber',
+          },
+        ],
+      },
+      {
+        title: 'PROTOCOLS AND INITIATIVES',
+        href: '/resource/protocols-and-initiatives',
+        hrefs: [
+          {
+            title: 'PROTOCOLS AND INITIATIVES',
+            href: '/resource/protocols-and-initiatives/protocols-and-initiatives',
+          },
+        ],
+      },
+      {
+        title: 'OPEN-SCIENCE GENERAL RESOURCES',
+        href: '/resource/open-science-general-resources',
+        hrefs: [
+          {
+            title: 'OPEN-SCIENCE GENERAL RESOURCES',
+            href: '/resource/open-science-general-resources/open-science-general-resources',
+          },
+        ],
+      },
+    ],
   },
   { href: '/contact', title: 'Contact' },
-  { href: '/people/member',
+  {
+    href: '/people/member',
     title: 'People',
     hrefs: [
       { title: 'Members', href: '/people/member' },
       { title: 'Collaborators', href: '/people/collaborator' },
-    ]
+    ],
   },
 ]
 
